085ddf83dce740c5d1be91453325cdb5b3f7837b
divyanshthakurx <divyanshthakurx@gmail.com>
2026-08-25 20:08:09 +0530

    feat(db): initialize database package with Prisma and Bun
    - Added index.ts to log a message when running the db package.
    - Updated package.json to include necessary dependencies and configurations for Bun and TypeScript.
    - Created .gitignore to exclude unnecessary files from version control.
    - Added README.md with instructions for installing dependencies and running the project.
    - Implemented database connection in index.ts using Prisma with PostgreSQL adapter.
    - Created prisma.config.ts for Prisma configuration.
    - Added initial migration files for User and Todo tables.
    - Defined Prisma schema with User and Todo models.
    - Included tsconfig.json for TypeScript configuration.

---
+++ packages/db/prisma/migrations/20260825143055_todo/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL,
+    "task" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
